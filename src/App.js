66692154d9e778c1772201e1b0110db152b28a61
Luis Feliz <Luisfeliz@gmail.com>
2021-02-06 09:48:59 -0500

    chore(ghPages): setup for deploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import AboutMe from "./pages/AboutMe/index"
 import Resume from "./pages/Resume/Resume"
 import Home from "./pages/Home/index"
